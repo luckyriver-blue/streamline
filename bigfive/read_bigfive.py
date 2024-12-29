7e42ed754c93154a345504ab5d8bdacf2f5f4f25
Yoshikawa Midori <yoshikawamidori@Yoshikawas-MacBook-Air.local>
2024-12-30 00:07:33 +0900

    ビッグフィアブデータのMEDIUMの範囲を修正しました。

--- bigfive/read_bigfive.py
+++ bigfive/read_bigfive.py
@@ -15,20 +15,23 @@ qualtrics_data = pd.read_csv(bigfive_csv, header=None)
 answer = qualtrics_data.iloc[-1, -11:-1]
 answer_list = answer.astype(int).tolist()
 
+#ビッグファイブのMEDIUMの範囲を平均 ± 標準偏差として定義する。
+#外向性は7.83±2.97、協調性は9.48±2.16、勤勉性は6.14±2.41、神経症傾向は9.21±2.48、開放性は8.03±2.48
+medium_range = [[4.86, 10.8], [7.32, 11.64], [3.73, 8.55], [6.73, 11.69], [5.55, 10.51]]
+
 #ビッグファイブをHIGH,MEDIUM,LOWに評価
-def score_bigfive(data, medium_range=(7, 9)):
+def score_bigfive(data):
   bigfive_list = []
   #計算方法は、参考になった論文のマニュアルから
   for i in range(5):
     #外向性、協調性、勤勉性、神経症傾向、開放性の順に入れる
     bigfive_list.append(data[i]+data[i+5]) 
   
-  #とりあえずMEDIUMを7以上9以下として出す。
   prompt_bigfive = []
-  for score in bigfive_list:
-    if score > medium_range[1]:
+  for i in range(5):
+    if bigfive_list[i] > medium_range[i][1]:
       prompt_bigfive.append('HIGH')
-    elif score >= medium_range[0]:
+    elif bigfive_list[i] >= medium_range[i][0]:
       prompt_bigfive.append('MEDIUM')
     else:
       prompt_bigfive.append('LOW')
