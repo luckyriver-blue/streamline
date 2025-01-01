0e51bbf8f05e8f46c43d648f5a0e95cbab46e760
Yoshikawa Midori <yoshikawamidori@Yoshikawas-MacBook-Air.local>
2025-01-02 01:38:14 +0900

    chat_aiファイルのインデントなどを少し修正しました。

--- chat_ai.py
+++ chat_ai.py
@@ -15,30 +15,30 @@ openness = prompt_bigfive[4]
 
 # プロンプトテンプレートの設定
 prompt_template = PromptTemplate(
-    input_variables=["history", "input"],
-    template=f"""
-    ユーザーの性格：
-    Extraversion: {extraversion}, 
-    Agreeableness: {agreeableness}, 
-    Conscientiousness: {conscientiousness}, 
-    Neuroticism: {neuroticism}, 
-    Openness: {openness}
+  input_variables=["history", "input"],
+  template=f"""
+  ユーザーの性格：
+  Extraversion: {extraversion}, 
+  Agreeableness: {agreeableness}, 
+  Conscientiousness: {conscientiousness}, 
+  Neuroticism: {neuroticism}, 
+  Openness: {openness}
 
-    ユーザーの性格を参照して、ユーザーにとって最適な会話を心がけてください。
-    ユーザーの性格のスコアには言及しないでください。
-    300文字以内で回答してください。
-    以下は会話の履歴です：\n{{history}}\n\nユーザーの入力：{{input}}
-    """
+  ユーザーの性格を参照して、ユーザーにとって最適な会話を心がけてください。
+  ユーザーの性格のスコアには言及しないでください。
+  300文字以内で回答してください。
+  以下は会話の履歴です：\n{{history}}\n\nユーザーの入力：{{input}}
+  """
 )
 
 
 conversation = ConversationChain(
-    llm=gpt, 
-    verbose=True, 
-    prompt=prompt_template,
-    memory=memory
+  llm=gpt, 
+  verbose=False, 
+  prompt=prompt_template,
+  memory=memory
 )
 
 #ユーザーの入力に対する応答を取得する関数
 def get_response(user_input):
-    return conversation.predict(input=user_input)
+  return conversation.predict(input=user_input)
