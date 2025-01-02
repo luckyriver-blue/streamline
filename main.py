8a9d8649520be65bccba25aa5292c38779c17241
Yoshikawa Midori <yoshikawamidori@Yoshikawas-MacBook-Air.local>
2025-01-02 21:59:54 +0900

    会話開始日前にビッグファイブデータアンケートに回答させるようにしました。

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 from config import gpt
+import datetime
 
 
 # Firebase Admin SDKの初期化
@@ -92,7 +93,6 @@ def add_data(collection_name, document_id, data):
   db.collection(collection_name).document(document_id).set(data, merge=True)
 
 
-
 #クエリパラメータからuser_idを取得（あれば）
 query_params = st.query_params
 if "user_id" in query_params:
@@ -109,6 +109,39 @@ if not st.session_state['user_id']:
       st.rerun()
     else:
       st.error("IDが間違っています")
+  st.stop()
+
+    
+
+#5日間の会話パート
+now = datetime.datetime.now()
+#会話パート開始日
+start_day = "2025-01-02" #仮
+start_day_obj = datetime.datetime.strptime(start_day, "%Y-%m-%d")
+#会話パート何日目か計算
+talk_day = (now - start_day_obj).days + 1
+ 
+#今日の日付が開始日よりも前の場合
+if now < start_day_obj:
+  #ビッグファイブのアンケートに回答してない場合は認証させてアンケートリンクを表示する
+  if prompt_bigfive == {}:
+    st.markdown(f'<a href="https://nagoyapsychology.qualtrics.com/jfe/form/SV_4N1LfAYkc9TrY8u?user_id={st.session_state["user_id"]}" target="_blank">こちら</a>をクリックしてアンケートに回答してください。', unsafe_allow_html=True)
+  #ビッグファイブのアンケートに回答済みの場合
+  else:
+    st.write(f"会話パートは{start_day_obj.month}月{start_day_obj.day}日15時から開始できます。")
+  st.stop()
+#5日間の後の場合
+elif talk_day > 5:
+  st.write("5日間の会話パートは終了しました。")
+  st.stop()
+#今の時間が午後3時よりも前の場合
+elif now.hour < 15:
+  st.write("会話は本日の15時から開始できます。")
+  st.stop()
+else:
+  st.title(f"会話{talk_day}日目")
+
+
 
 if st.session_state['user_id']:
   # 会話メッセージの履歴を表示
@@ -117,7 +150,12 @@ if st.session_state['user_id']:
       st.markdown(message["content"])
 
   if st.session_state.count == 5:
-    add_data('users', st.session_state['user_id'], {"messages": st.session_state["messages"]})
+    add_data('users', st.session_state['user_id'], {"messages": {"day": day, "messages": st.session_state["messages"]}})
+    if day < 5:
+      st.markdown('これで今日の会話は終了です。')
+    else:
+      st.markdown('これで5日間の会話パートは終了です。<a href="https://qualtricsxm3s9qj93mr.qualtrics.com/login#/surveys/SV_9Y4Ye1aiZf5nfy6>こちら</a>をクリックしてアンケートに回答してください。', unsafe_allow_html=True)
+
     st.markdown('これで本日の会話は終了です。')
 
   #ユーザーの入力
